e7278b604001ae99bdd7665473bdf136e08e7f50
Samuel Solomon <mrsamuelsolomon@gmail.com>
2022-06-09 10:34:14 +0100

    feat: create catcard

--- next.config.js
+++ next.config.js
@@ -1,6 +1,15 @@
 /** @type {import('next').NextConfig} */
+
 const nextConfig = {
   reactStrictMode: true,
+  images: {
+    domains: ['i.pravatar.cc'], // what domains are we allowing to pull images from
+  },
+  i18n: {
+    locales: ['en', 'fr'],
+    defaultLocale: 'en',
+    // localeDetection: false,
+  },
 };
 
 module.exports = nextConfig;
